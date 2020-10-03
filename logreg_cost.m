3d45eebd751b12b75b99905ec82743afda913ad2
theSLWayne <hiranhasanka@gmail.com>
2020-10-03 16:42:09 +0530

    Add prediction algorithm

--- logreg_cost.m
+++ logreg_cost.m
@@ -15,7 +15,7 @@ function [cost] = logreg_cost(X, y, theta, lambda);
   % Regularization Parameters
   % First line of thetas, theta0, doesn't get regularized. Filtering them out to calculate regularization parameters.
   reg_thetas = theta(2:end, 1);
-  reg_param = lambda / (2 * m) * (reg_thetas' .* reg_thetas);
+  reg_param = lambda / (2 * m) * (reg_thetas' * reg_thetas);
   
   % Calculate Regularized Cost function
   cost = (-1 / m) * (y' * log(preds) + (1 - y)' * log(1 - preds)) + reg_param;
